f605c5697036722dc8f6cbb7eca3bd65def6ef09
Давлат Ушурбакиев <dushurbakiev@gmail.com>
2023-04-02 09:47:55 +0600

    added migration

---
+++ migrations/m230402_034506_create_todo_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%todo_items}}`.
+ */
+class m230402_034506_create_todo_items_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%todo_item}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->text()->notNull(),
+            'priority' => $this->integer()->notNull()->defaultValue(0),
+            'done' => $this->boolean()->notNull()->defaultValue(false),
+            'created_at' => $this->dateTime()->notNull(),
+            'updated_at' => $this->dateTime()->notNull(),
+            'version' => $this->integer()->notNull()->defaultValue(1),
+        ]);
+
+        // Create index on priority column
+        $this->createIndex('idx-todo_item-priority', '{{%todo_item}}', 'priority');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%todo_items}}');
+    }
+}
